93165807471100087a5d66a1b790d1ad9f3f8146
charan <charan9487@gmail.com>
2020-06-18 22:31:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector("#ham").addEventListener("click",function(){
+	console.log("hi");
+	document.querySelector(".navbar").classList.add("new");
+	console.log(document.querySelector(".nav_link"))
+})
+
+document.querySelector("#close").addEventListener("click",function(){
+	console.log("hi");
+	document.querySelector(".navbar").classList.remove("new");
+})
