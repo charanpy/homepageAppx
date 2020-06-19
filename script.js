c60a0a98d2d41f9641f885e4aafa062615693510
charanpy <58394919+charanpy@users.noreply.github.com>
2020-06-19 07:25:32 +0530

    script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.querySelector("#ham").addEventListener("click",function(){
 	console.log("hi");
 	document.querySelector(".navbar").classList.add("new");
 	console.log(document.querySelector(".nav_link"))
+document.querySelector(".Appx-feauture").classList.toggle("none");
 })
 
 document.querySelector("#close").addEventListener("click",function(){
